e70ef287fe0e3d62496a31fec5221719f3b8d21b
unicov_tg <unicov.the.gamer@gmail.com>
2021-02-26 02:41:48 +0900

    起動時にパスを取得・セットする

---
+++ DarkSouls3DataBackupper/Libs/PathUtility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DarkSouls3DataBackupper.Libs
+{
+    class PathUtility
+    {
+        /// <summary>
+        /// ダークソウル3のセーブデータパスを取得
+        /// </summary>
+        /// <returns>セーブデータのパス</returns>
+        public static string GetDS3SaveDataPath()
+        {
+            var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var path = Path.Combine(appDataPath, "DarkSoulsIII");
+            var saveDataDirectoryPath = Directory.GetDirectories(path).First();
+
+            return saveDataDirectoryPath;
+        }
+    }
+}
